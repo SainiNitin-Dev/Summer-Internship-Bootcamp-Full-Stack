e823106fe43626c246f39d2a3196f6e9ac580696
ItsNitinSaini <142825241+ItsNitinSaini@users.noreply.github.com>
2026-06-16 14:18:31 +0530

    React Day 3 Before Break

---
+++ React Day 3/day3proj/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import UseEffectOne from './UseEffectOne'
+
+const App = () => {
+  return (
+    <div>
+      <UseEffectOne />
+    </div>
+  )
+}
+
+export default App
